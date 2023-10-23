5aae8e5c5ac0d6d987b608a7b8c7b44e0887090f
viswanathgowda <viswavishu40@gmail.com>
2023-10-23 19:17:22 +0530

    routing requests

--- nodejs_01/app.js
+++ nodejs_01/app.js
@@ -1,7 +1,14 @@
 const http = require("http")
 const server = http.createServer((req, res)=>{
-    console.log(req.url, req.method, req.headers)
     res.setHeader('content-type', 'text/html');
+    const url = req.url
+    if(url === '/'){
+        res.write('<html>')
+        res.write('<head><title>Enter Message</title></head>')
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button></form></body>')
+        res.write('</html>')
+        return res.end()
+    }
     res.write('<html>')
     res.write('<head><title>my first page</title></head>')
     res.write('<body><h1>hellow from nodejs</h1></body>')
