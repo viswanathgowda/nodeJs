6831b7d31b389873013fe2c71d4ccd8baab37c2c
viswanathgowda <viswavishu40@gmail.com>
2023-10-23 19:38:27 +0530

    redirecting requests

--- nodejs_01/app.js
+++ nodejs_01/app.js
@@ -1,7 +1,8 @@
 const http = require("http")
+const fs = require("fs")
 const server = http.createServer((req, res)=>{
-    res.setHeader('content-type', 'text/html');
     const url = req.url
+    const method = req.method
     if(url === '/'){
         res.write('<html>')
         res.write('<head><title>Enter Message</title></head>')
@@ -9,6 +10,13 @@ const server = http.createServer((req, res)=>{
         res.write('</html>')
         return res.end()
     }
+    if(url === '/message' && method === "POST"){
+        fs.writeFileSync('message.txt', 'DUMMY')
+        res.statusCode = 302;
+        res.setHeader('Location', '/') 
+        return res.end();
+    }
+    res.setHeader('content-type', 'text/html');
     res.write('<html>')
     res.write('<head><title>my first page</title></head>')
     res.write('<body><h1>hellow from nodejs</h1></body>')
