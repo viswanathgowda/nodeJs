f8a8b310a334c0347bd718d5d5fa878b431895ba
viswanathgowda <viswavishu40@gmail.com>
2023-10-23 19:03:43 +0530

    sending response from nodejs server

--- nodejs_01/app.js
+++ nodejs_01/app.js
@@ -1,5 +1,11 @@
 const http = require("http")
 const server = http.createServer((req, res)=>{
     console.log(req.url, req.method, req.headers)
+    res.setHeader('content-type', 'text/html');
+    res.write('<html>')
+    res.write('<head><title>my first page</title></head>')
+    res.write('<body><h1>hellow from nodejs</h1></body>')
+    res.write('</html>')
+    res.end()
 })
 server.listen(3000)
