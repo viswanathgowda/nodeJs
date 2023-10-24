49d7fcfda5e0b239a88e71bb263e8da8001057a1
viswanathgowda <viswavishu40@gmail.com>
2023-10-24 10:31:11 +0530

    route file using node module system

--- nodejs_01/app.js
+++ nodejs_01/app.js
@@ -1,35 +1,7 @@
 const http = require("http")
-const fs = require("fs")
-const server = http.createServer((req, res)=>{
-    const url = req.url
-    const method = req.method
-    if(url === '/'){
-        res.write('<html>')
-        res.write('<head><title>Enter Message</title></head>')
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button></form></body>')
-        res.write('</html>')
-        return res.end()
-    }
-    if(url === '/message' && method === "POST"){
-        const body = []
-        req.on('data', (chunk)=>{
-            body.push(chunk)
-        })
-        return req.on('end', ()=>{
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1]
-            fs.writeFileSync('message.txt', message)
-            res.statusCode = 302;
-            res.setHeader('Location', '/') 
-            return res.end();
-        })
-       
-    }
-    res.setHeader('content-type', 'text/html');
-    res.write('<html>')
-    res.write('<head><title>my first page</title></head>')
-    res.write('<body><h1>hellow from nodejs</h1></body>')
-    res.write('</html>')
-    res.end()
-})
+
+const routes = require('./router')
+console.log(routes.someText)
+
+const server = http.createServer(routes.handler)
 server.listen(3000)
