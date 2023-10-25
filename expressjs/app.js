a84d753129b4b233841bf38c8d5d92959057f778
viswanathgowda <viswavishu40@gmail.com>
2023-10-25 16:24:45 +0530

    adding stylings and calling the static files using express.static method

--- expressjs/app.js
+++ expressjs/app.js
@@ -7,6 +7,7 @@ const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 app.use(bodyparser.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/admin',adminRoutes)//**filtering the paths */
 
