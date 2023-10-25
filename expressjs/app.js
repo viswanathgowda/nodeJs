0ab63f7e6bde20a99b799f491ce645df943a8c84
viswanathgowda <viswavishu40@gmail.com>
2023-10-25 11:54:10 +0530

    handling different routes in expressjs

--- expressjs/app.js
+++ expressjs/app.js
@@ -2,12 +2,16 @@ const express = require("express")
 
 const app = express()
 
-app.use((req, res, next)=>{
+app.use('/',(req, res, next)=>{
     console.log('in the middle ware')
-    next() //**it will call the next middle ware */
+    next() //**it will call the next middle ware, all middle ware works from top to bottom, if not given the next() callback method other middle ware wont execute, also even after sending the response in any of the middle ware if we call next() callback it will throw an error */
 })
-app.use((req, res, next)=>{
+app.use('/add-product',(req, res, next)=>{
     console.log('next middle ware')
+    res.send("<h1>add product</h1>")
+})
+app.use('/',(req, res, next)=>{
+    console.log('in the middle ware')
     res.send("<h1>hello from expressjs</h1>")
 })
 
