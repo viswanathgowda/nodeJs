3eae3e0df9ffef2d966a760759ffcbde334132b8
viswanathgowda <viswavishu40@gmail.com>
2023-10-25 14:05:14 +0530

    creating 404 page in expressjs

--- expressjs/app.js
+++ expressjs/app.js
@@ -11,5 +11,9 @@ app.use(adminRoutes)
 
 app.use(shopRoutes)
 
+app.use((req, res, next)=>{
+    res.status(404).send('<h1>page not found</h1>') //**this method used for 404 page and if setted all the routes with respective pages */
+})
+
 
 app.listen(3000) //**it will work same  const server = http.createServer(routes.handler)  server.listen(3000)*/
--- expressjs/routes/shop.js
+++ expressjs/routes/shop.js
@@ -2,7 +2,7 @@ const express = require("express")
 
 const router = express.Router()
 
-router.use('/',(req, res, next)=>{
+router.get('/',(req, res, next)=>{
     res.send("<h1>hello from expressjs</h1>")
 })
 
