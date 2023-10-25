b72c6c76bef8eb18b2f1e6c3bc0e5ca2c7a5fb13
viswanathgowda <viswavishu40@gmail.com>
2023-10-25 14:49:19 +0530

    creating and serving html pages in expressjs

--- expressjs/routes/admin.js
+++ expressjs/routes/admin.js
@@ -1,11 +1,11 @@
+const path = require("path")
 const express = require("express")
-const { route } = require("./shop")
 
 const router = express.Router()
 
 //**admin path */
 router.get('/add-product',(req, res, next)=>{
-    res.send('<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>')
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
 })
 router.post('/product',(req, res, next)=>{
     console.log(req.body)
