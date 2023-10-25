acbb1e827adc551adaf55e9eb7985c7abc30ade1
viswanathgowda <viswavishu40@gmail.com>
2023-10-25 11:18:31 +0530

    how middle wares

--- expressjs/app.js
+++ expressjs/app.js
@@ -9,6 +9,7 @@ app.use((req, res, next)=>{
 })
 app.use((req, res, next)=>{
     console.log('next middle ware')
+    res.send("<h1>hello from expressjs</h1>")
 })
 
 const server = http.createServer(app)
